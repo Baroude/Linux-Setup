50501b52acb2e2893be2fdb4761d8590db042883
Baroude <67190983+Baroude@users.noreply.github.com>
2022-10-04 11:53:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 cd ~
 git clone https://github.com/neovim/neovim
-cd neovim && make 
+cd neovim && make CMAKE_BUILD_TYPE=Release
 sudo make install
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
